4595df87550faba55fb81ee355bb595875fbb810
shiva kushwah <shivakushwah144@gmail.com>
2025-08-28 12:06:46 +0530

    update deps

--- Learnex/backend/index.js
+++ Learnex/backend/index.js
@@ -9,8 +9,8 @@ import cors from "cors";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
-// const secret = "shhh";
+const PORT = process.env.PORT ;
+
 
 app.use(cors());
 app.use(
